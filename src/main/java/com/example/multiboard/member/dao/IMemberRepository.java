df5f1f25c1cd71e2da479d838ab1ddf70eb4cd79
gnawH <sun30126331@gmail.com>
2026-04-03 14:50:50 +0900

    feat: add IMemberRepository Interface class

---
+++ src/main/java/com/example/multiboard/member/dao/IMemberRepository.java
@@ -0,0 +1,18 @@
+package com.example.multiboard.member.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface IMemberRepository {
+
+	void insertMember(Member member);
+	Member selectMember(String userid);
+	List<Member> selectAllMembers();
+	void updateMember(Member member);
+	void deleteMember(Member member);
+	String getPassword(String userid);
+}
